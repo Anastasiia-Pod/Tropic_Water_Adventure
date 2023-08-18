ae3f7db3724459b0cdaad91633355b86ea7a8de2
Anastasiia Podopryhora <anastasia95podoprigora@gmail.com>
2023-08-18 21:15:40 +0200

    component and obstacle classes created

--- Tropical_Water_Adventure/js/game.js
+++ Tropical_Water_Adventure/js/game.js
@@ -12,10 +12,10 @@ class Game {
       this.gameIsOver = false;
       this.player = new Player(
         this.gameScreen,
-        300,
-        600,
         200,
-        250,
+        500,
+        220,
+        300,
         "./Images/Boy.png"
       );
     }
@@ -27,18 +27,16 @@ class Game {
   
       // Hide the start screen
       this.startScreen.style.display = "none";
-      
       // Show the game screen
       this.gameScreen.style.display = "block";
   
-      // Start the game loop
+      //
       this.gameLoop();
     }
   
     gameLoop() {
       console.log("in the game loop");
   
-      // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
       if (this.gameIsOver) {
         return;
       }
@@ -49,6 +47,59 @@ class Game {
     }
   
     update() {
-        this.player.move();
+      this.player.move();
+  
+      // Check for collision and if an obstacle is still on the screen
+      for (let i = 0; i < this.obstacles.length; i++) {
+        const obstacle = this.obstacles[i];
+        obstacle.move();
+  
+        // If the player's windsurf board collides with an obstacle
+        if (this.player.didCollide(obstacle)) {
+          // Remove the obstacle element from the DOM
+          obstacle.element.remove();
+          // Remove obstacle object from the array
+          this.obstacles.splice(i, 1);
+          // Reduce player's lives by 1
+          this.lives--;
+          // Update the counter variable to account for the removed obstacle
+          i--;
+        } // If the obstacle is off the screen (at the bottom)
+        else if (obstacle.top > this.height) {
+          // Increase the score by 1
+          this.score++;
+          // Remove the obstacle from the DOM
+          obstacle.element.remove();
+          // Remove obstacle object from the array
+          this.obstacles.splice(i, 1);
+          // Update the counter variable to account for the removed obstacle
+          i--;
+        }
+      }
+  
+      // If the lives are 0, end the game
+      if (this.lives === 0) {
+        this.endGame();
+      }
+  
+      // Create a new obstacle based on a random probability
+      // when there is no other obstacles on the screen
+      if (Math.random() > 0.98 && this.obstacles.length < 1) {
+        this.obstacles.push(new Obstacle(this.gameScreen));
+      }
     }
-}  
+  
+    // Create a new method responsible for ending the game
+    endGame() {
+      this.player.element.remove();
+      this.obstacles.forEach(function (obstacle) {
+        obstacle.element.remove();
+      });
+  
+      this.gameIsOver = true;
+      // Hide game screen
+      this.gameScreen.style.display = "none";
+      // Show end game screen
+      this.gameEndScreen.style.display = "block";
+    }
+  }
